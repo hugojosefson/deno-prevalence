8ca43b62dd5445f1e4251438884d7669f96e84e5
Hugo Josefson <hugo@josefson.org>
2023-10-07 21:11:13 +0200

    fixup! refactor: extract in-memory tests

--- test/in-memory/prevalence-in-memory.test.ts
+++ test/in-memory/prevalence-in-memory.test.ts
@@ -5,32 +5,28 @@ import { Action } from "../../mod.ts";
 import { AddPostAction } from "../add-post-action.ts";
 import { AddUserAction } from "../add-user-action.ts";
 import { MyModel, Post, User } from "../fixture-types.ts";
-import { test } from "./test-case.ts";
+import { test } from "../test-case.ts";
+
+const kv = () => Deno.openKv(":memory:");
 
 describe("prevalence in :memory:", () => {
-  test((system) => assertEquals(system.name, "test"));
-  test((system) => assertEquals(system.model.posts.length, 2));
-  test((system) => assertEquals(system.model.users.length, 26));
-  test((system) => assertInstanceOf(system.model.posts[0], Post));
-  test((system) => assertInstanceOf(system.model.users[0], User));
-  test(
-    "Add a post",
-    async (system) => {
-      const post = new Post("3", "Goodbye");
-      const action: Action<MyModel> = new AddPostAction(post);
-      await system.execute(action);
-      assertEquals(system.model.posts.length, 3);
-      assertEquals(system.model.posts[2], post);
-    },
-  );
-  test(
-    "Add a user",
-    async (system) => {
-      const user = new User("27", "Zelda");
-      const action: Action<MyModel> = new AddUserAction(user);
-      await system.execute(action);
-      assertEquals(system.model.users.length, 27);
-      assertEquals(system.model.users[26], user);
-    },
-  );
+  test(kv, (system) => assertEquals(system.name, "test"));
+  test(kv, (system) => assertEquals(system.model.posts.length, 2));
+  test(kv, (system) => assertEquals(system.model.users.length, 26));
+  test(kv, (system) => assertInstanceOf(system.model.posts[0], Post));
+  test(kv, (system) => assertInstanceOf(system.model.users[0], User));
+  test(kv, "Add a post", async (system) => {
+    const post = new Post("3", "Goodbye");
+    const action: Action<MyModel> = new AddPostAction(post);
+    await system.execute(action);
+    assertEquals(system.model.posts.length, 3);
+    assertEquals(system.model.posts[2], post);
+  });
+  test(kv, "Add a user", async (system) => {
+    const user = new User("27", "Zelda");
+    const action: Action<MyModel> = new AddUserAction(user);
+    await system.execute(action);
+    assertEquals(system.model.users.length, 27);
+    assertEquals(system.model.users[26], user);
+  });
 });
--- test/test-case.ts
+++ test/test-case.ts
@@ -1,19 +1,21 @@
 import { it } from "https://deno.land/std@0.203.0/testing/bdd.ts";
 import { using as usingResource } from "https://deno.land/x/websocket_broadcastchannel@0.8.0/src/using.ts";
-import { Prevalence } from "../../src/prevalence.ts";
-import { MyModel } from "../fixture-types.ts";
-import { getFixtureCreator } from "../get-fixture.ts";
+import { Prevalence, PrevalenceOptions } from "../src/prevalence.ts";
+import { MyModel } from "./fixture-types.ts";
+import { getFixtureCreator } from "./get-fixture.ts";
 
-const createFixture = getFixtureCreator(() => Deno.openKv(":memory:"));
+export type TestCase = (system: Prevalence<MyModel>) => Promise<void> | void;
+export type Kv = PrevalenceOptions<MyModel>["kv"];
 
-type TestCase = (system: Prevalence<MyModel>) => Promise<void> | void;
-
-export function test(name: string, testCase: TestCase): void;
-export function test(testCase: TestCase): void;
+export function test(kv: Kv, name: string, testCase: TestCase): void;
+export function test(kv: Kv, testCase: TestCase): void;
 export function test(
+  kv: Kv,
   testCaseOrName: TestCase | string,
   testCase?: TestCase,
 ): void {
+  const createFixture = getFixtureCreator(kv);
+
   const hasName = typeof testCaseOrName === "string";
   const hasNameAndTestCase = hasName && testCase !== undefined;
 
